e078b23c5703c599809742c3b9623ba41fa05076
Aayush Patel <patel.aayush2@northeastern.edu>
2024-10-09 07:08:18 -0400

    fixed formating according to status check

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ resource "aws_internet_gateway" "my_igw" {
 }
 
 resource "aws_subnet" "public_subnets" {
-  count = length(var.public_subnet_cidrs)
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.public_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
 
   tags = {
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_subnets" {
 }
 
 resource "aws_subnet" "private_subnets" {
-  count = length(var.private_subnet_cidrs)
+  count             = length(var.private_subnet_cidrs)
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = var.private_subnet_cidrs[count.index]
   availability_zone = var.availability_zones[count.index]
